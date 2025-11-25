47185a3f3f042726f478549e3eb71eb2f07a21bd
kphudson13 <kphudson@live.ca>
2025-11-25 00:08:32 -0500

    Collapse first node and add some phyla

--- SupportCode/BuildTree.R
+++ SupportCode/BuildTree.R
@@ -5,18 +5,16 @@ library(tidytree)
 library(tidyverse)
 library(ggtree)
 
-
 # install.packages("BiocManager")
 # BiocManager::install("ggtree") # need these for ggtree 
 
 Descriptions <- read.csv("AnimalTreeApp/InvertDescriptions.csv") # call in the groups we cant
 
-
 taxa <- tnrs_match_names(Descriptions$Clade[
   (Descriptions$Level == "Superphylum" |
-    Descriptions$Level == "Phylum" | 
-    Descriptions$Level == "Subphylum" | 
-    Descriptions$Level == "Class") & 
+     Descriptions$Level == "Phylum" | 
+     Descriptions$Level == "Subphylum" | 
+     Descriptions$Level == "Class") & 
     Descriptions$Clade != "Medusozoa"], # medusozoa gives errors, will in later 
   context_name = "Animals") # Pull taxa
 
@@ -27,31 +25,59 @@ tree <- tol_induced_subtree(ott_ids = taxa$ott_id) # Build Tree
 tree$tip.label <- strip_ott_ids(tree$tip.label, remove_underscores = T) # remove ids from tip labels 
 
 # rename things 
-tree$node.label[tree$node.label == "mrcaott42ott49"] <- "Nephrozoa "
-tree$node.label[tree$node.label == "Lophotrochozoa ott155737"] <- "Spiralia "
+tree$node.label[tree$node.label == "mrcaott42ott49"] <- "Nephrozoa " # space after cause its a node
 tree$node.label[tree$node.label == "mrcaott42ott658"] <- "Chordata "
 tree$node.label[tree$node.label == "mrcaott56ott519"] <- "Lophotrochozoa "
 tree$node.label[tree$node.label == "mrcaott431ott3524"] <- "Medusozoa "
-tree$tip.label[tree$tip.label == "Ctenophora (phylum ncbi:10197)"] <- "Ctenophora"
+tree$node.label[tree$node.label == "mrcaott49ott6612"] <- "Spiralia "
+tree$node.label[tree$node.label == "Lophotrochozoa ott155737"] <- NA # named clades we dont want
+tree$node.label[tree$node.label == "Pancrustacea ott985906"] <- NA
+tree$node.label[tree$node.label == "Panarthropoda ott816442"] <- NA
+tree$node.label[tree$node.label == "Ambulacraria ott6520512"] <- NA
+tree$tip.label[tree$tip.label == "Ctenophora (phylum ncbi:10197)"] <- "Ctenophora" # no space for tips
 tree$tip.label[tree$tip.label == "Vertebrata (subphylum in Deuterostomia)"] <- "Vertebrata"
 tree$tip.label[tree$tip.label == "mrcaott150ott7012"] <- "Hexacorallia"
+tree$tip.label[tree$tip.label == "Onychophora (phylum in Holozoa)"] <- "Onychophora"
 
 #remove useless node labels
 tree$node.label <- ifelse(grepl(" ", tree$node.label), str_extract(tree$node.label, "^[^ ]+"), "")
 tree$node.label[tree$node.label == ""] <- NA 
 
-colnames(Descriptions)[colnames(Descriptions) == "Clade"] = "label" # i think this is necessary 
+# If the tree has no branch lengths, create them, this is just so we can collapse polytomies
+if (is.null(tree$edge.length)) {
+  tree$edge.length <- rep(1, nrow(tree$edge))  # give all edges length 1
+}
 
+
+pairs_to_collapse <- list(
+  c("Ctenophora", "Porifera"),
+  c("Platyhelminthes", "Rotifera"),
+  c("Chaetognatha", "Rotifera"),
+  c("Platyhelminthes", "Chaetognatha")
+)
+
+for (pair in pairs_to_collapse) {
+  node <- getMRCA(tree, pair)
+  if (!is.na(node)) {
+    edges_to_zero <- which(tree$edge[,1] == node)
+    tree$edge.length[edges_to_zero] <- 0
+  }
+}
+
+tree <- di2multi(tree)
+
+colnames(Descriptions)[colnames(Descriptions) == "Clade"] = "label" # i think this is necessary to match in desriptions
+
+# reorder for the sake of the legend 
 Descriptions$Level <- factor(Descriptions$Level, levels = c("Higher Clade", "Superphylum", "Phylum", "Subphylum", "Class"))
 
 TreePlot <- ggtree(tree, branch.length="none", aes(color=Level), size = 1.5) %<+% Descriptions + # match descriptions to nodes
   geom_tiplab(fill="white", geom = "label", size = 5, fontface = 2) +
   geom_nodelab(subset = !is.na(node.label), 
-        fill="white", geom = "label", size = 5, fontface = 2) +
-      theme(legend.position = c(0.1,0.8),
-            legend.title = element_blank(),
-            legend.text = element_text(size=16))
-    
-    save(TreePlot, file = "AnimalTreeApp/ggTreeObject") # save it so the app can call it
-    
-    
+               fill="white", geom = "label", size = 5, fontface = 2) +
+  theme(legend.position = c(0.1,0.8),
+        legend.title = element_blank(),
+        legend.text = element_text(size=16))
+
+save(TreePlot, file = "AnimalTreeApp/ggTreeObject") # save it so the app can call it
+
